715169d0b4628e025ec27663b827e188aafcad0c
Anishka Srivastava <anishkasrivastava339@gmail.com>
2025-07-31 22:16:41 +0530

    Calculator.py

---
+++ Dashboard/home.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.markdown ('<h1 style="color: red; background-color:white;  text-align: center">Titanic Dashboard</h1>',unsafe_allow_html=True)
+st.image('Assets\Titanic.jpg', use_column_width=True)
